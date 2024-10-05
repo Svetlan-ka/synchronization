d0136130be6cadf93e4ddc21796383fb5d17883e
Svetlan-ka <sv.chapova@yandex.ru>
2024-10-06 00:12:29 +0700

    update freqlog

--- src/main/java/org/example/DeliveryRobot.java
+++ src/main/java/org/example/DeliveryRobot.java
@@ -1,48 +1,74 @@
 package org.example;
 
 import java.util.*;
-import java.util.concurrent.Callable;
 
 public class DeliveryRobot {
     public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
     public static void countNumberCommands() throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
+        List<Thread> threads1 = new ArrayList<>();
+        List<Thread> threads2 = new ArrayList<>();
 
         String[] routes = new String[1000];
         for (int i = 0; i < routes.length; i++) {
             routes[i] = generateRoute("RLRFR", 100).concat(" ");
+//            System.out.println(routes[i]);
         }
 
         for (String route : routes) {
-            Thread thread = new Thread(() -> {
+            Thread thread1 = new Thread(() -> {
                 int freqHitR = 0; //частота попадания символа
 
-                for (int i = 0; i < route.length(); i++) {
-                    char c = route.charAt(i);
+                synchronized (sizeToFreq) {
+                    for (int i = 0; i < route.length(); i++) {
+                        char c = route.charAt(i);
 
-                    if (c == 'R') {
-                        freqHitR++;
-                    } else if (freqHitR != 0) {
-                        synchronized (sizeToFreq) {   //ПРОБЛЕМА ЗДЕСЬ
+                        if (c == 'R') {
+                            freqHitR++;
+                        } else if (freqHitR != 0) {
+
                             if (sizeToFreq.containsKey(freqHitR)) {
                                 int newCount = sizeToFreq.get(freqHitR) + 1; //увеличиваем кол-во частот
                                 sizeToFreq.put(freqHitR, newCount);
+                                sizeToFreq.notify();
                             } else {
                                 sizeToFreq.put(freqHitR, 1); //создаем новую частоту
+                                sizeToFreq.notify();
                             }
                             freqHitR = 0;
+
                         }
                     }
+
                 }
             });
-            threads.add(thread);
-            thread.start();
+            threads1.add(thread1);
+            thread1.start();
+
+            Thread thread2 = new Thread(() -> {
+                synchronized (sizeToFreq) {
+                    while (!Thread.interrupted()) {
+                        try {
+                            sizeToFreq.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    int currentFreqLeader = Collections.max(sizeToFreq.entrySet(), Map.Entry.comparingByValue()).getKey();
+                    System.out.printf("Текущий лидер среди частот %d (встретиласть %d раз)\n", currentFreqLeader, sizeToFreq.get(currentFreqLeader));
+                }
+            });
+            threads2.add(thread2);
+            thread2.start();
+
+            for (Thread thread : threads1) {
+                thread.join();
+                thread2.interrupt();
+            }
+            for (Thread thread : threads2) {
+                thread.join();
+            }
         }
-        for (Thread thread : threads) {
-            thread.join();
-        }
-        printResult();
     }
 
     public static void printResult() {
